855f8cf737ee48aeb11afddfaf6aabe319f0fc84
hardly <hardly@digital.gov.mg>
2020-12-28 11:59:19 +0300

    28122020

--- static/src/js/mgp.js
+++ static/src/js/mgp.js
@@ -2,16 +2,14 @@ odoo.define('mgp.plainte', function (require) {
     "use strict";
 
     var FormView = require('web.FormView');
-    
 
-    
     FormView.include({
-        
-        
-        init: function() {
+        init: function(record) {
             this._super.apply(this, arguments);
             console.log('INIT');
-            
+            console.log(record.fields.reference);
+
+            self = this;
             if (this.controllerParams.modelName === 'mgp.plainte') {
                 //var plainte = new Model('mgp.plainte');
                 //var data = this.model.get(this.handle);
@@ -25,14 +23,63 @@ odoo.define('mgp.plainte', function (require) {
                 
                 //o_form_button_save
                 //class=oe_edit_only
-             }
+            }
+        },
+
+        start: function() {
+            this._super.apply(this, arguments);
+            self = this;
+            console.log('START');
+        },
+
+        load_record: function(record) {
+            this._super.apply(this, arguments);
+            self = this;
+            console.log('LOAD RECORD');
+            
+            // if (this.model === 'mgp.plainte') {
+            //     if (this.datarecord && (this.datarecord.statut === 'state')) {
+            //         this.$buttons.find('.o_form_button_edit').css({'display':'none'});
+            //     }
+            //     else {
+            //         this.$buttons.find('.o_form_button_edit').css({'display':''});
+            //     }
+            // }
         },
     });
 });
 
+// odoo.define('mgp.plainte', function (require) {
+//     "use strict";
+
+//     var FormView = require('web.FormView');
+
+//     FormView.include({
+        
+//         init: function() {
+//             this._super.apply(this, arguments);
+//             console.log('INIT');
+            
+//             if (this.controllerParams.modelName === 'mgp.plainte') {
+//                 //var plainte = new Model('mgp.plainte');
+//                 //var data = this.model.get(this.handle);
+//                 // var fields = this.field_manager;
+//                 // console.log(fields);
+
+//                 // if (this.datarecord && (this.datarecord.statut === 'state')) {
+//                 //     this.rendererParams.activeActions.edit = false;
+//                 // }      
+//                 //this.rendererParams.activeActions.edit = false;          
+                
+//                 //o_form_button_save
+//                 //class=oe_edit_only
+//              }
+//         },
+//     });
+// });
+
 //DESACTIVER LE BUTTON EDIT
-// odoo.define('mgp.plainte', function (1require) {
-    
+// odoo.define('mgp.plainte', function (require) {
     
 //     var FormView = require('web.FormView');
 
